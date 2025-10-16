cbd58cc61f7fda713de166e66e89582daa5f6e2a
Mr.Mushroom <abraameskander1@gmail.com>
2025-10-16 13:32:41 +1100

    Update start.py

--- genai-studio/backend/start.py
+++ genai-studio/backend/start.py
@@ -1,1 +1,4 @@
-import uvicorn; uvicorn.run("app.main:app", reload=True, port=8000)
+import uvicorn
+
+if __name__ == '__main__':
+    uvicorn.run("app.main:app", reload=False, port=8000, host="127.0.0.1")
